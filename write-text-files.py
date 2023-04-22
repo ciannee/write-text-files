8b87df21dc73b7d2966274492bd3b684bdb728b8
Patricia Pineda <pepineda28@gmail.com>
2023-04-23 00:29:50 +0800

    initial commit (1)

--- write-text-files.py
+++ write-text-files.py
@@ -1,6 +1,6 @@
 # Name: Pineda, Patricia Anne E.
 # Section: BSCPE 1-5
-#  Assignment 4: Programming Exercise 3 (Extract Even and Odd Numbers)
+#  Assignment 4: Programming Exercise 3 (Multiple Line of Text Contents into a Text File)
 
 # Write a method in python to write multiple line of text contents into 
 # a text file mylife.txt. See sample output:
